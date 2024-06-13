ba5b9e3d45e906d60e09f6e6a059b8209da0daf3
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-13 22:04:33 +0530

    5th script

--- 05-variable.sh
+++ 05-variable.sh
@@ -6,4 +6,4 @@ echo "please enter your username:: "
 read -s USERNAME
 echo "please enter your password:: "
 read -s PASSWORD
-echo "username is:$USERNAME, password is:$PASSWORD"
+#echo "username is:$USERNAME, password is:$PASSWORD"
