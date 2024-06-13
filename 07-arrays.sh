29d70a275df5cea471b7a0c3a3fd3d6243eadc39
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-13 22:43:37 +0530

    5th script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-FRUITS=("Apple" "Banana" "Mango")
-echo " First value: ${FRUITS[0]}"
-echo " second value: ${FRUITS[1]}"
-echo " third value: ${FRUITS[2]}"
-echo " all fruits value: ${FRUITS[@]}"
+#FRUITS=("Apple" "Banana" "Mango")
+#echo " First value: ${FRUITS[0]}"
+#echo " second value: ${FRUITS[1]}"
+#echo " third value: ${FRUITS[2]}"
+#echo " all fruits value: ${FRUITS[@]}"
+
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "Total:: $SUM"
+
+echo "How many args passed:: $#"
+
+echo "All args passed:: $@"
+
+echo "Script name:: $0"
