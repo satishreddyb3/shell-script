3db89a66992e01c63d43157b0f37c0f115c6be4e
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-14 07:13:14 +0530

    5th script

---
+++ 10-functions.sh
@@ -0,0 +1,21 @@
+ID=$(id -u)
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR:: $2 failed"
+        exit 1
+    else
+        echo "$2 SUCCESS"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi
+yum install mysql -y
+VALIDATE $? "installing sql"
+yum install git -y
+VALIDATE $? "installing git"
