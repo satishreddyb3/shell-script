8614941f95966417db7fa4a24ea21a69a3432f34
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-14 12:05:18 +0530

    5th script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,32 +1,37 @@
+#!/bin/bash
+
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ... $R FAILED $N"
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
     else
         echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1
+    exit 1 # you can give other than 0
 else
-    echo "you are root user"
-f1
-echo "All arguments passed:$@"
-for package in $@
-do
-    yum list installed $package
-    if [ $? -ne 0 ]
-    then
-        yum install $package -y 
-        VALIDATE $? "Installion of $package"
-    else
-        echo -e "$package is already installed  ... $Y SKIPPING $N"
-    fi
-done
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $? "Installing MySQL"
+
+yum install git -y &>> $LOGFILE
+
+VALIDATE $? "Installing GIT"
