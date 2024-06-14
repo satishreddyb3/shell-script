d28eb70919652d393a7ab3419789297e7b6e1e98
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-14 12:07:19 +0530

    5th script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "ERROR:: $2 ... $R FAILED $N"
-        exit 1
+        echo -e "$2 ... $R FAILED $N"
     else
         echo -e "$2 ... $G SUCCESS $N"
     fi
@@ -28,10 +28,18 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-yum install mysql -y &>> $LOGFILE
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
 
-VALIDATE $? "Installing MySQL"
-
-yum install git -y &>> $LOGFILE
-
-VALIDATE $? "Installing GIT"
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE #check installed or not
+    if [ $? -ne 0 ] #if not installed
+    then
+        yum install $package -y &>> $LOGFILE # install the package
+        VALIDATE $? "Installation of $package" # validate
+    else
+        echo -e "$package is already installed ... $Y SKIPPING $N"
+    fi
+done
