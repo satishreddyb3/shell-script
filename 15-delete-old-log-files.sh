21248991ac0869aeb000f4f7e4b26c84731a8129
satishreddyb3 <satishreddyb3@gmail.com>
2024-06-17 07:08:33 +0530

    shellscript

--- 15-delete-old-log-files.sh
+++ 15-delete-old-log-files.sh
@@ -1,21 +1,4 @@
 #!bin/bash
-:'
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-SOURCE_DIR=/tmp/shellscript-logs
-if [ ! -d $SOURCE_DIR ]
-then
-    echo -e "$R soure directory not exit : $SOURCE_DIR does not exit. $N
-fi
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-while IFS= read -r line
-do
-    echo "Deleting file: $line"
-done <<< $FILES_TO_DELETE
-'
-
 SOURCE_DIR="/tmp/shellscript-logs"
 
 R="\e[31m"
